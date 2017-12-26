92322273b16366ae30e0c239df6bfaaf2a6ab958
xfgczzg <xfgczzg@163.com>
2017-12-26 22:05:21 +0800

    字符串加解密功能添加，代码是复制的别人的，仅供参考,在原CMakeList中添加了c文件的支持,可以正常跑起来

--- ndk-sample/src/main/java/com/example/ndk/sample/MainActivity.java
+++ ndk-sample/src/main/java/com/example/ndk/sample/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private final String LOG_TAG = "test";
     private SampleJni sampleJni;
+
+    private TextView textView;
 
     static {
         System.loadLibrary("hello");
@@ -25,13 +29,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         /*卸载监听*/
         sampleJni.callUnInstallListener(Build.VERSION.SDK_INT, "data/data/com.example.ndk.sample");
 
+        initView();
         addListener();
+    }
+
+    private void initView() {
+        textView = findViewById(R.id.tv_desc);
     }
 
     private void addListener() {
         findViewById(R.id.btn_action_calc).setOnClickListener(this);
-        findViewById(R.id.btn_action_decode).setOnClickListener(this);
-        findViewById(R.id.btn_action_encode).setOnClickListener(this);
+        findViewById(R.id.btn_action_edcode).setOnClickListener(this);
         findViewById(R.id.btn_action_videodecode).setOnClickListener(this);
         findViewById(R.id.btn_action_videoencode).setOnClickListener(this);
         findViewById(R.id.btn_action_piczip).setOnClickListener(this);
@@ -43,12 +51,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.btn_action_calc:
                 String calcResult = "相加的结果: " + sampleJni.add(15, 12) + "";
-                Log.e("test", calcResult);
+                Log.e(LOG_TAG, calcResult);
                 Toast.makeText(this, calcResult, Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.btn_action_encode:
-                break;
-            case R.id.btn_action_decode:
+            case R.id.btn_action_edcode:
+                String str = "13550110110";
+                String encodeStr = sampleJni.encode(str);
+                String decodeStr = sampleJni.decode(encodeStr);
+                textView.setText("加密前：" + str + "\n加密后：" + encodeStr + "\n解密后：" + decodeStr);
                 break;
             case R.id.btn_action_videodecode:
                 break;
